5a5a50917c04601a088e0cb65ec9f04cbed592cf
SwarajPawar <swarajpawar23@gmail.com>
2021-11-27 21:02:56 -0500

    Update

--- run_rat_spn_fashion_mnist.py
+++ run_rat_spn_fashion_mnist.py
@@ -87,7 +87,7 @@ def run():
                 cmd += " --timeout_seconds {}".format(remaining_time)
                 cmd += " --split_depth {}".format(split_depth)
                 cmd += " --data_path data/fashion-mnist/"
-                cmd += " --data_set fashion_mnist/"
+                cmd += " --data_set fashion_mnist"
                 cmd += " --result_path results/fashion-mnist/"
 
                 for key in sorted(structure_config.keys()):
